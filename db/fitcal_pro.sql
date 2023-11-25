50bfed163741909b99eb0f6abc6c56d7a5d00da1
darren813 <darrenchen813@gmail.com>
2023-11-25 16:40:41 -0500

    did mealtracker, profile, daytracker, measurements, workouttracker, routine, exercise

--- db/fitcal_pro.sql
+++ db/fitcal_pro.sql
@@ -1,57 +1,108 @@
 -- testing: write to here
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0;
+SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0;
+SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'TRADITIONAL,ALLOW_INVALID_DATES';
 
 
-DROP DATABASE IF EXISTS `fitcal` ;
-CREATE DATABASE IF NOT EXISTS `fitcal` DEFAULT CHARACTER SET latin1 ;
-USE `fitcal` ;
+DROP DATABASE IF EXISTS `fitcal`;
+CREATE DATABASE IF NOT EXISTS `fitcal` DEFAULT CHARACTER SET latin1;
+USE `fitcal`;
 
 
 CREATE TABLE IF NOT EXISTS Store (
-   storeID INT AUTO_INCREMENT PRIMARY KEY,
-   name VARCHAR(50),
-   rating FLOAT,
-   country VARCHAR(50),
-   city VARCHAR(50),
-   zip INT,
-   street VARCHAR(50)
+    storeID INT AUTO_INCREMENT PRIMARY KEY,
+    name    VARCHAR(50),
+    rating  FLOAT,
+    country VARCHAR(50),
+    city    VARCHAR(50),
+    zip     INT,
+    street  VARCHAR(50)
 );
 
 
 CREATE TABLE IF NOT EXISTS Vegan_Alternatives (
-   alternativeID INT AUTO_INCREMENT PRIMARY KEY,
-   name VARCHAR(50),
-   price DECIMAL(10, 2), # supports up to $99,999,999.99
-   calories INT,
-   quantity INT
+    alternativeID INT AUTO_INCREMENT PRIMARY KEY,
+    name          VARCHAR(50),
+    price         DECIMAL(10, 2), # supports up to $99,999,999.99
+    calories      INT,
+    quantity      INT
 );
 
 
 CREATE TABLE IF NOT EXISTS Nutrients (
-   nutrientID INT AUTO_INCREMENT PRIMARY KEY,
-   name VARCHAR(50)
+    nutrientID INT AUTO_INCREMENT PRIMARY KEY,
+    name       VARCHAR(50)
 );
 
 
 CREATE TABLE IF NOT EXISTS Ingredients (
-   ingredientID INT AUTO_INCREMENT PRIMARY KEY,
-   name VARCHAR(50),
-   price DECIMAL(10, 2), # supports up to $99,999,999.99
-   calories INT,
-   quantity INT
+    ingredientID INT AUTO_INCREMENT PRIMARY KEY,
+    name         VARCHAR(50),
+    price        DECIMAL(10, 2), # supports up to $99,999,999.99
+    calories     INT,
+    quantity     INT
 );
 
 
 CREATE TABLE IF NOT EXISTS Recipes (
-   recipeID INT AUTO_INCREMENT PRIMARY KEY,
-   name VARCHAR(50),
-   rating FLOAT,
-   servingSize INT,
-   allergens TEXT,
-   calories INT,
-   timeToMake TIME,
-   steps TEXT
+    recipeID    INT AUTO_INCREMENT PRIMARY KEY,
+    name        VARCHAR(50),
+    rating      FLOAT,
+    servingSize INT,
+    allergens   TEXT,
+    calories    INT,
+    timeToMake  TIME,
+    steps       TEXT
 );
+
+CREATE TABLE IF NOT EXISTS MealTracker (
+    mealTrackerID INT AUTO_INCREMENT PRIMARY KEY,
+    mealDateTime  DATETIME
+);
+
+CREATE TABLE IF NOT EXISTS Exercise (
+    exerciseID INT AUTO_INCREMENT PRIMARY KEY,
+    name       VARCHAR(50),
+    weight     INT,
+    reps       INT,
+    difficulty INT, # 1-10
+    equipment  VARCHAR(50),
+    targetArea VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS Routine (
+    routineID    INT AUTO_INCREMENT PRIMARY KEY,
+    name         VARCHAR(50),
+    difficulty   INT, # 1-10
+    timeDuration TIME
+);
+
+CREATE TABLE IF NOT EXISTS WorkoutTracker (
+    workoutID     INT AUTO_INCREMENT PRIMARY KEY,
+    timeDuration  TIME,
+    caloriesBurnt INT
+);
+
+CREATE TABLE IF NOT EXISTS Measurements (
+    measureID   INT AUTO_INCREMENT PRIMARY KEY,
+    waterIntake INT,
+    hoursSlept  INT,
+    feetHeight  INT, # the 5 in 5'10
+    inchHeight  INT, # the 10 in 5/10
+    weight      INT  # lbs
+);
+
+CREATE TABLE IF NOT EXISTS DayTracker (
+    dayTrackerID INT AUTO_INCREMENT PRIMARY KEY,
+    date         DATE
+);
+
+CREATE TABLE IF NOT EXISTS Profile (
+    username         VARCHAR(50) AUTO_INCREMENT PRIMARY KEY,
+    name             VARCHAR(50),
+    bio              TEXT,
+    registerDateTime DATETIME,
+    birthDateTime    DATETIME
+);
+
 

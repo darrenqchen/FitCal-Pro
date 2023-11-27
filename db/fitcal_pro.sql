ac60f9078b39f0cedf9dfbdd3bf32a1f03fa11b9
billxu1401 <billxu1401@gmail.com>
2023-11-26 20:56:56 -0500

    updated tables and added bridge tables

--- db/fitcal_pro.sql
+++ db/fitcal_pro.sql
@@ -9,7 +9,8 @@ CREATE DATABASE IF NOT EXISTS `fitcal` DEFAULT CHARACTER SET latin1;
 USE `fitcal`;
 
 
-CREATE TABLE IF NOT EXISTS Store (
+CREATE TABLE IF NOT EXISTS Store
+(
     storeID INT AUTO_INCREMENT PRIMARY KEY,
     name    VARCHAR(50),
     rating  FLOAT,
@@ -19,60 +20,91 @@ CREATE TABLE IF NOT EXISTS Store (
     street  VARCHAR(50)
 );
 
-
-CREATE TABLE IF NOT EXISTS VeganAlternatives (
-    alternativeID INT AUTO_INCREMENT PRIMARY KEY,
-    name          VARCHAR(50),
-    price         DECIMAL(10, 2), # supports up to $99,999,999.99
-    calories      INT,
-    quantity      INT
-);
-
-
-CREATE TABLE IF NOT EXISTS Nutrients (
+CREATE TABLE IF NOT EXISTS Nutrients
+(
     nutrientID INT AUTO_INCREMENT PRIMARY KEY,
-    name       VARCHAR(50)
+    name       VARCHAR(50),
+    grams      INT
 );
 
-
-CREATE TABLE IF NOT EXISTS Ingredients (
+CREATE TABLE IF NOT EXISTS Ingredients
+(
     ingredientID INT AUTO_INCREMENT PRIMARY KEY,
-    name         VARCHAR(50),
+    name         INT,
     price        DECIMAL(10, 2), # supports up to $99,999,999.99
     calories     INT,
-    quantity     INT
+    quantity     INT,
+    isVegan      BOOLEAN,
+    FOREIGN KEY (name) REFERENCES Nutrients (nutrientID) ON UPDATE cascade ON DELETE cascade
 );
 
-CREATE TABLE IF NOT EXISTS VeganTips (
+CREATE TABLE IF NOT EXISTS Store_Ingredients
+(
+    storeID      INT,
+    ingredientID INT,
+    PRIMARY KEY (storeID, ingredientID),
+    FOREIGN KEY (storeID) REFERENCES Store (storeID) ON UPDATE cascade ON DELETE cascade,
+    FOREIGN KEY (ingredientID) REFERENCES Ingredients (ingredientID) ON UPDATE cascade ON DELETE cascade
+);
+
+CREATE TABLE IF NOT EXISTS VeganTips
+(
     tipID INT AUTO_INCREMENT PRIMARY KEY,
-    tip TEXT
+    tip   TEXT
 );
 
-CREATE TABLE IF NOT EXISTS Restaurant (
-    restaurantID INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50),
-    cuisine VARCHAR(50),
-    rating INT, # 1-10
-    street VARCHAR(50),
-    city VARCHAR(50),
-    country VARCHAR(50),
-    zipcode INT
-);
-
-CREATE TABLE IF NOT EXISTS VeganMeals (
-    veganMealID INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50),
-    calories INT
-);
-
-CREATE TABLE IF NOT EXISTS Meals (
-    veganMealID INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50),
-    calories INT
+CREATE TABLE IF NOT EXISTS VeganTips_Store
+(
+    tipID   INT,
+    storeID INT,
+    PRIMARY KEY (tipID, storeID),
+    FOREIGN KEY (tipID) REFERENCES VeganTips (tipID) ON UPDATE cascade ON DELETE cascade,
+    FOREIGN KEY (storeID) REFERENCES Store (storeID) ON UPDATE cascade ON DELETE cascade
 );
 
 
-CREATE TABLE IF NOT EXISTS Recipes (
+CREATE TABLE IF NOT EXISTS Restaurant
+(
+    restaurantID    INT AUTO_INCREMENT PRIMARY KEY,
+    name            VARCHAR(50),
+    cuisine         VARCHAR(50),
+    rating          INT, # 1-10
+    locationStreet  VARCHAR(50),
+    locationCity    VARCHAR(50),
+    locationCountry VARCHAR(50),
+    locationZip     INT
+);
+
+CREATE TABLE IF NOT EXISTS VeganTips_Restaurant
+(
+    tipID   INT,
+    restaurantID INT,
+    PRIMARY KEY (tipID, restaurantID),
+    FOREIGN KEY (tipID) REFERENCES VeganTips (tipID) ON UPDATE cascade ON DELETE cascade,
+    FOREIGN KEY (restaurantID) REFERENCES Restaurant (restaurantID) ON UPDATE cascade ON DELETE cascade
+);
+
+CREATE TABLE IF NOT EXISTS Meals
+(
+    mealID INT AUTO_INCREMENT PRIMARY KEY,
+    name        VARCHAR(50),
+    calories    INT,
+    ingredients INT,
+    isVegan BOOLEAN,
+    FOREIGN KEY (ingredients) REFERENCES Ingredients (ingredientID) ON UPDATE cascade ON DELETE cascade
+);
+
+CREATE TABLE IF NOT EXISTS Meals_Restaurant
+(
+    mealID      INT,
+    restaurantID INT,
+    PRIMARY KEY (mealID, restaurantID),
+    FOREIGN KEY (mealID) REFERENCES Meals (mealID) ON UPDATE cascade ON DELETE cascade,
+    FOREIGN KEY (restaurantID) REFERENCES Restaurant (restaurantID) ON UPDATE cascade ON DELETE cascade
+);
+
+CREATE TABLE IF NOT EXISTS Recipes
+(
     recipeID    INT AUTO_INCREMENT PRIMARY KEY,
     name        VARCHAR(50),
     rating      FLOAT,
@@ -83,12 +115,14 @@ CREATE TABLE IF NOT EXISTS Recipes (
     steps       TEXT
 );
 
-CREATE TABLE IF NOT EXISTS MealTracker (
+CREATE TABLE IF NOT EXISTS MealTracker
+(
     mealTrackerID INT AUTO_INCREMENT PRIMARY KEY,
     mealDateTime  DATETIME
 );
 
-CREATE TABLE IF NOT EXISTS Exercise (
+CREATE TABLE IF NOT EXISTS Exercise
+(
     exerciseID INT AUTO_INCREMENT PRIMARY KEY,
     name       VARCHAR(50),
     weight     INT,
@@ -98,20 +132,23 @@ CREATE TABLE IF NOT EXISTS Exercise (
     targetArea VARCHAR(50)
 );
 
-CREATE TABLE IF NOT EXISTS Routine (
+CREATE TABLE IF NOT EXISTS Routine
+(
     routineID    INT AUTO_INCREMENT PRIMARY KEY,
     name         VARCHAR(50),
     difficulty   INT, # 1-10
     timeDuration TIME
 );
 
-CREATE TABLE IF NOT EXISTS WorkoutTracker (
+CREATE TABLE IF NOT EXISTS WorkoutTracker
+(
     workoutID     INT AUTO_INCREMENT PRIMARY KEY,
     timeDuration  TIME,
     caloriesBurnt INT
 );
 
-CREATE TABLE IF NOT EXISTS Measurements (
+CREATE TABLE IF NOT EXISTS Measurements
+(
     measureID   INT AUTO_INCREMENT PRIMARY KEY,
     waterIntake INT,
     hoursSlept  INT,
@@ -120,17 +157,17 @@ CREATE TABLE IF NOT EXISTS Measurements (
     weight      INT  # lbs
 );
 
-CREATE TABLE IF NOT EXISTS DayTracker (
+CREATE TABLE IF NOT EXISTS DayTracker
+(
     dayTrackerID INT AUTO_INCREMENT PRIMARY KEY,
     date         DATE
 );
 
-CREATE TABLE IF NOT EXISTS Profile (
+CREATE TABLE IF NOT EXISTS Profile
+(
     username         VARCHAR(50) AUTO_INCREMENT PRIMARY KEY,
     name             VARCHAR(50),
     bio              TEXT,
     registerDateTime DATETIME,
     birthDateTime    DATETIME
 );
-
-

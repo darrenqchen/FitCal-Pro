a33d2b120a80786f69c408d12c638ab5681cd998
mdevine6427 <152667667+mdevine6427@users.noreply.github.com>
2023-12-04 19:21:25 -0500

    Added excercise ID endpoint

--- flask-app/src/exercises/exercises.py
+++ flask-app/src/exercises/exercises.py
@@ -19,3 +19,19 @@ def get_exercises():
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+
+@excercises.route('/excercises/<id>', methods=['GET'])
+def get_excercise_detail(id):
+
+    query = 'SELECT id, name, weight, reps, difficulty, equipment, targetArea FROM Exercises WHERE id = ' + str(id)
+    current_app.logger.info(query)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    column_headers = [x[0] for x in cursor.description]
+    json_data = []
+    the_data = cursor.fetchall()
+    for row in the_data:
+        json_data.append(dict(zip(column_headers, row)))
+    return jsonify(json_data)

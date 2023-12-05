ba36c5ebc478806ab08fcff1035f01879806933f
billxu1401 <billxu1401@gmail.com>
2023-12-05 11:04:03 -0500

    added get and post for nutrients

--- flask-app/src/meals/meals.py
+++ flask-app/src/meals/meals.py
@@ -45,6 +45,21 @@ def get_recipe_detail(id):
    query = 'SELECT recipeID, name, rating, servingSize, allergens, calories, timeToMake, steps, isVegan FROM Recipes WHERE recipeID = ' + str(id)
    current_app.logger.info(query)
 
+   cursor = db.get_db().cursor()
+   cursor.execute(query)
+   column_headers = [x[0] for x in cursor.description]
+   json_data = []
+   the_data = cursor.fetchall()
+   for row in the_data:
+       json_data.append(dict(zip(column_headers, row)))
+   return jsonify(json_data)
+
+
+# Gets a certain nutrient from the DB
+@meals.route('/nutrients/<id>', methods=['GET'])
+def get_nutrient_detail(id):
+   query = 'SELECT nutrientID, name, grams FROM Nutrients WHERE nutrientID = ' + str(id)
+   current_app.logger.info(query)
 
    cursor = db.get_db().cursor()
    cursor.execute(query)
@@ -176,6 +191,31 @@ def add_new_recipe():
    query += str(timeToMake) + ', "'
    query += steps + '", '
    query += str(isVegan) + ')'
+   current_app.logger.info(query)
+
+   # executing and committing the insert statement
+   cursor = db.get_db().cursor()
+   cursor.execute(query)
+   db.get_db().commit()
+  
+   return 'Success!'
+
+
+# Add a new nutrient to DB
+@meals.route('/nutrients', methods=['POST'])
+def add_new_nutrient():
+   # collecting data from the request object
+   the_data = request.json
+   current_app.logger.info(the_data)
+
+   #extracting the variable
+   name = the_data['name']
+   grams = the_data['grams']
+
+   # Constructing the query
+   query = 'insert into Nutrients (name, grams) values ("'
+   query += name + '", '
+   query += str(grams) + ')'
    current_app.logger.info(query)
 
    # executing and committing the insert statement

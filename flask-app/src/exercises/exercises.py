cfe3c13a746bfa06b658664375a55055f652912c
Darren <darrenchen813@gmail.com>
2023-12-04 20:52:33 -0500

    exerciseID changed

--- flask-app/src/exercises/exercises.py
+++ flask-app/src/exercises/exercises.py
@@ -2,7 +2,6 @@ from flask import Blueprint, request, jsonify, make_response, current_app
 import json
 from src import db
 
-
 exercises = Blueprint('exercises', __name__)
 
 # Get all exercises from the DB
@@ -21,11 +20,11 @@ def get_exercises():
     return the_response
 
 # Gets a certain exercise from the DB
-@exercises.route('/excercises/<id>', methods=['GET'])
+@exercises.route('/exercises/<id>', methods=['GET'])
 def get_excercise_detail(id):
-    query = 'SELECT id, name, weight, reps, difficulty, equipment, targetArea FROM Exercises WHERE id = ' + str(id)
+    query = 'SELECT exerciseID, name, weight, reps, difficulty, equipment, targetArea FROM Exercises WHERE id = ' + str(id)
     current_app.logger.info(query)
-    
+
     cursor = db.get_db().cursor()
     cursor.execute(query)
     column_headers = [x[0] for x in cursor.description]

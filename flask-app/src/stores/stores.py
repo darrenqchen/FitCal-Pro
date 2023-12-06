89c39ccbe1b8f3cfe7fc94ab1374f28544550378
Kerbs <kerber.el@northeastern.edu>
2023-12-05 20:05:42 -0500

    Added restaurant functions

--- flask-app/src/stores/stores.py
+++ flask-app/src/stores/stores.py
@@ -82,3 +82,68 @@ def add_new_store():
     db.get_db().commit()
     
     return 'Success!'
+#------------Restaurants----------
+# Get all resturants from the DB
+@stores.route('/restaurants', methods=['GET'])
+def get_restaurantID():
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT restaurantID, name, rating, street, city, zip, country FROM Restaurants')
+    column_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
+
+# Get Restaurants details from a particular restaurantID
+@stores.route('/restaurants/<restaurantID>', methods=['GET'])
+def get_restaurantID_detail(restaurantID):
+    query = 'SELECT restaurantID, name, rating, street, city, zip, country FROM Restaurants WHERE restaurantID = ' + str(restaurantID)
+    current_app.logger.info(query)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    column_headers = [x[0] for x in cursor.description]
+    json_data = []
+    the_data = cursor.fetchall()
+    for row in the_data:
+        json_data.append(dict(zip(column_headers, row)))
+    return jsonify(json_data)
+
+# Adding a store
+@stores.route('/restaurants', methods=['POST'])
+def add_new_restaurant():
+    
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
+    #extracting the variable
+    name = the_data['name']
+    cuisine = the_data['cuisine']
+    rating = the_data['rating']
+    street = the_data['street']
+    city = the_data['city']
+    zip = the_data['zip']
+    country = the_data['country']
+    
+
+    # Constructing the query
+    query = 'insert into Restaurants (name, cuisine, rating, street, city, zip, country) values ("'
+    query += name + '", "'
+    query += cuisine + '", "'
+    query += str(rating) + '", "'
+    query += street + '", "'
+    query += city + '", "'
+    query += zip + '", "'
+    query += country + '")'
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+    
+    return 'Success!'

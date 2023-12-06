676862daa82d9cc7f05bbff1838d17bde0a4ec61
JackWhiskey <75497398+JackWhiskey@users.noreply.github.com>
2023-12-05 23:28:56 -0500

    Measurements endpoint

--- flask-app/src/measurements/measurements.py
+++ flask-app/src/measurements/measurements.py
@@ -23,19 +23,85 @@ def get_measurements():
     return jsonify(json_data)
 
 # Add current measurement
-@measurements.route('/measurement/', methods=['POST'])
+@measurements.route('/measurement', methods=['POST'])
 def new_measurement():
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    # extracting the variable
+    waterIntake = the_data['waterIntake']
+    hoursSlept = the_data['hoursSlept']
+    heightFeet = the_data['heightFeet']
+    heightInches = the_data['heightInches']
+    weight = the_data['weight']
+    dayTrackerID = the_data['dayTrackerID']
+
+    query = 'INSERT INTO Measurements (waterIntake, hoursSlept, heightFeet, heigthInches, weight, dayTrackerID) values ('
+    query += str(waterIntake) + ', '
+    query += str(hoursSlept) + ', '
+    query += str(heightFeet) + ', '
+    query += str(heightInches) + ', '
+    query += str(weight) + ', '
+    query += str(dayTrackerID) + ')'
+    current_app.logger.info(query)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return 'Success!'
 
 # /measurements/{measureID}
 
 # Get specified measurement
 @measurements.route('/measurements/<id>', methods=['GET'])
 def get_measurement(id):
+    query = 'SELECT measureID, waterIntake, hoursSlept, heightFeet, heigthInches, weight, dayTrackerID FROM Measurements WHERE measureID = ' + str(id)
+    current_app.logger.info(query)
+    
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    column_headers = [x[0] for x in cursor.description]
+    json_data = []
+    the_data = cursor.fetchall()
+    for row in the_data:
+        json_data.append(dict(zip(column_headers, row)))
+    return jsonify(json_data)
 
 # Change specified measurement
 @measurements.route('/measurements/<id>', methods=['POST'])
 def change_measurement(id):
 
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    # extracting the variable
+    waterIntake = the_data['waterIntake']
+    hoursSlept = the_data['hoursSlept']
+    heightFeet = the_data['heightFeet']
+    heightInches = the_data['heightInches']
+    weight = the_data['weight']
+    dayTrackerID = the_data['dayTrackerID']
+
+    # Constructing the query
+    query = f'UPDATE Measurements SET waterIntake = {waterIntake}, hoursSlept = {hoursSlept}, heightFeet = {heightFeet}, heightInches = {heightInches}, weight = {weight}, dayTrackerID = {dayTrackerID} WHERE measureID = {id};'
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
 # Delete specified measurement
 @measurements.route('/measurement/<id>', methods=['DELETE'])
 def delete_measurement(id):
+    query = '''
+       DELETE
+       FROM Measurements
+       WHERE measureID = {0};
+   '''.format(id)
+    
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+
+    db.get_db().commit()
+    return  "Successfully deleted workout tracker #{0}!".format(id)
